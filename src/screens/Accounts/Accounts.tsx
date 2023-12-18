ec4ec61171f17d80ac3c707b4d5efee79b8862f8
Angel G <angelguzmaning@gmail.com>
2023-12-18 10:23:49 -0400

    Show account balance

--- src/screens/Accounts/Accounts.tsx
+++ src/screens/Accounts/Accounts.tsx
@@ -4,35 +4,54 @@ import { ActivityIndicator, Text, Button, Divider } from "react-native-paper";
 import { FlatList, TouchableOpacity, View } from "react-native";
 import { useAccounts } from "../../services/accountService";
 import { usePullDownRefresh } from "../../hooks/usePullDownRefresh";
+import { Account } from "../../../types/account";
+import { P, match } from "ts-pattern";
 
 export function AccountsScreen({ navigation }: ScreenProps<"Accounts">) {
-  const { data, isLoading, refetch } = useAccounts();
-  const { refreshing, onRefresh } = usePullDownRefresh(refetch);
+  const accountsQuery = useAccounts();
+  const { refreshing, onRefresh } = usePullDownRefresh(accountsQuery.refetch);
 
-  useRefreshOnFocus(refetch);
+  useRefreshOnFocus(accountsQuery.refetch);
 
-  if (isLoading || refreshing || data === undefined) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
+  if (accountsQuery.error) {
+    console.error(accountsQuery.error);
   }
 
   return (
     <View style={{ padding: 24, gap: 12 }}>
       <Button onPress={() => navigation.navigate("NewAccount")}>New Account</Button>
-      <FlatList
-        data={data}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => navigation.navigate("AccountRecords", { account: item })}>
-            <View style={{ padding: 12 }}>
-              <Text variant="displaySmall">{item.name}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-        ItemSeparatorComponent={Divider}
-        refreshing={refreshing}
-        onRefresh={onRefresh}
-        contentContainerStyle={{ paddingBottom: 24 }}
-      />
+      {match(accountsQuery)
+        .with({ isLoading: true }, () => <ActivityIndicator size="large" color="#0000ff" />)
+        .with({ data: undefined }, () => <Text style={{ color: "red" }}>Something went wrong</Text>)
+        .with({ error: P.not(P.nullish) }, () => (
+          <Text style={{ color: "red" }}>An error ocurred, please try again later</Text>
+        ))
+        .otherwise(({ data }) => (
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => navigation.navigate("AccountRecords", { account: item })}>
+                <AccountPanel account={item} />
+              </TouchableOpacity>
+            )}
+            ItemSeparatorComponent={Divider}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            contentContainerStyle={{ paddingBottom: 24 }}
+          />
+        ))}
+    </View>
+  );
+}
+
+function AccountPanel({ account }: { account: Account }) {
+  return (
+    <View style={{ padding: 12, flexDirection: "row", justifyContent: "space-between" }}>
+      <Text variant="displaySmall">{account.name}</Text>
+      <Text variant="labelLarge" style={{ verticalAlign: "bottom" }}>
+        ${account.balance.toFixed(2)}
+      </Text>
     </View>
   );
 }
--- types/account.ts
+++ types/account.ts
@@ -3,9 +3,11 @@ import { Decoder, number, object, string } from "decoders";
 export interface Account {
   id: number;
   name: string;
+  balance: number;
 }
 
 export const accountDecoder: Decoder<Account> = object({
   id: number,
   name: string,
+  balance: number,
 });
